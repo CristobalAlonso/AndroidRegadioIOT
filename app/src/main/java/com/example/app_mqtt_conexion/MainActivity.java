ed3d6d10f102446084917fb5eca1142886453ad9
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-11-08 15:51:22 -0300

    base project

--- app/src/main/java/com/example/app_mqtt_conexion/MainActivity.java
+++ app/src/main/java/com/example/app_mqtt_conexion/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     MqttAndroidClient client;              //  clienteMQTT este dispositivo
     MqttConnectOptions options;            // para meter parametros a la conexion
+    private TextView txt;                  //text view para mostrar en interfacve
 
 
    @Override
@@ -51,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
        //options.setUserName(USERNAME);
        //options.setPassword(PASSWORD.toCharArray());
        checar_conexion();//revisamos la conexion
+       txt = findViewById(R.id.textView);
+
     }
     private void obtener_nombre_Dispositivo() {
         String fabricante = Build.MANUFACTURER;
@@ -78,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // mensaje de conectado
                     Toast.makeText(getBaseContext(), "Conectado ", Toast.LENGTH_SHORT).show();
+                    sub("Wl/ldr");
                 }
 
                 @Override//si falla la conexion
@@ -111,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    public  void  publicarD1 (View v) {
-        String tema="st";///corrrespode al tema de LED
-        String  menssage="ON";
-        publicaste= "publicaras " + tema + " " + menssage; //concatenamos el dato a publicar
+    private void publish(String topic, String menssage)
+    {
+        //String tema="st";///corrrespode al tema de LED
+        //String  menssage="ON";
+        publicaste= "publicaras " + topic + " " + menssage; //concatenamos el dato a publicar
         intento_publicar=true;//si intento publicar
         checar_conexion();//revisamos la conexion
 
@@ -127,32 +131,88 @@ public class MainActivity extends AppCompatActivity {
                 // 1:se envia hasta garantizar la entrega en caso de fallo resive duplicados
                 //2: se  garantiza que se entrege al subcribtor unicamente una vez
                 //retenid=false;//true es que el mensaje se quede guardado en el broker asta su actualizacion
-                client.publish(tema, menssage.getBytes(),qos, false);
+                client.publish(topic, menssage.getBytes(),qos, false);
                 Toast.makeText(getBaseContext(), publicaste, Toast.LENGTH_SHORT).show();
             }catch (Exception e){e.printStackTrace();}
         }
-
     }
-    public  void  publicarD2 (View v) {
-        String tema="st";///corrrespode al tema de LED
-        String  menssage="OFF";
-        publicaste= "publicaras " + tema + " " + menssage; //concatenamos el dato a publicar
-        intento_publicar=true;//si intento publicar
-        checar_conexion();//revisamos la conexion
 
-        if (permiso_publicar){
+    public void sub(String topic)
+    {
+        try
+        {
+            client.subscribe(topic, 0);
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
 
-            try {
-                int qos=0;//indica la prioridad del mensaje.
-                // 0:envio una vez,
-                // 1:se envia hasta garantizar la entrega en caso de fallo resive duplicados
-                //2: se  garantiza que se entrege al subcribtor unicamente una vez
-                //retenid=false;//true es que el mensaje se quede guardado en el broker asta su actualizacion
-                client.publish(tema, menssage.getBytes(),qos, false);
-                Toast.makeText(getBaseContext(), publicaste, Toast.LENGTH_SHORT).show();
-            }catch (Exception e){e.printStackTrace();}
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String msg = null;
+                    if(topic.matches("Wl/ldr"))
+                    {
+                        msg =  new String(message.getPayload());
+                        txt.setText(msg+" lux");
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+
+                }
+            });
+        }
+        catch (MqttException e){
+            e.printStackTrace();
         }
 
+
+
     }
+
+    //led 1
+    public void publicarD1oN(View view)
+    {
+        publish("Wl/01","1");
+    }
+    public void publicarD1Off(View view)
+    {
+        publish("Wl/01","0");
+    }
+
+    //led 2
+    public void publicarD2On(View view)
+    {
+        publish("Wl/02","1");
+    }
+    public void publicarD2Off(View view)
+    {
+        publish("Wl/02","0");
+    }
+
+    //led 3
+    public void publicarD3On(View view)
+    {
+        publish("Wl/03","1");
+    }
+    public void publicarD3Off(View view)
+    {
+        publish("Wl/03","0");
+    }
+
+    //led 4
+    public void publicarD4On(View view)
+    {
+        publish("Wl/04","1");
+    }
+    public void publicarD4Off(View view)
+    {
+        publish("Wl/04","0");
+    }
+
+
+
 
 }
